9aac7bfb7a860dd98c72acbaaf09be4c02ba3629
Your Name <you@example.com>
2022-05-05 11:43:58 -0400

    new try

--- heap.c
+++ heap.c
@@ -47,7 +47,15 @@ void heap_push(Heap* pq, void* data, int priority){
 
 
 void heap_pop(Heap* pq){
-
+   if(pq->size==0) return NULL;
+   free(pq->heapArray[0].data);
+   int aux=0;
+   while(aux!=pq->size-1){
+      pq->heapArray[aux].data=pq->heapArray[aux+1].data;
+      pq->heapArray[aux].priority=pq->heapArray[aux+1].priority;
+      aux=aux+1;
+   }   
+   pq->size=aux;
 }
 
 Heap* createHeap(){
